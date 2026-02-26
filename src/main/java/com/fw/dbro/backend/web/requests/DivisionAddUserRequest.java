a64e849e128443c3012cc912664ec6258c65ace2
fw <thatgloomyafternoon@gmail.com>
2026-02-27 01:05:08 +0700

    progress

---
+++ src/main/java/com/fw/dbro/backend/web/requests/DivisionAddUserRequest.java
@@ -0,0 +1,31 @@
+package com.fw.dbro.backend.web.requests;
+
+import java.util.UUID;
+import javax.validation.constraints.NotNull;
+import static com.fw.dbro.backend.constants.MessageConstants.*;
+
+public class DivisionAddUserRequest {
+
+  @NotNull(message = DIVISION_ID_NOT_NULL_MESSAGE)
+  private UUID divisionId;
+
+  @NotNull(message = USER_AUTH_ID_NOT_NULL_MESSAGE)
+  private UUID userAuthId;
+
+  public UUID getDivisionId() {
+    return divisionId;
+  }
+
+  public void setDivisionId(UUID divisionId) {
+    this.divisionId = divisionId;
+  }
+
+  public UUID getUserAuthId() {
+    return userAuthId;
+  }
+
+  public void setUserAuthId(UUID userAuthId) {
+    this.userAuthId = userAuthId;
+  }
+
+}
