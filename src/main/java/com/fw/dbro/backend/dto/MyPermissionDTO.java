8999c28150ac0ae528293f5c910acdf4252c12fa
fw <thatgloomyafternoon@gmail.com>
2026-03-10 01:42:45 +0700

    progress

---
+++ src/main/java/com/fw/dbro/backend/dto/MyPermissionDTO.java
@@ -0,0 +1,24 @@
+package com.fw.dbro.backend.dto;
+
+public class MyPermissionDTO {
+
+  private String apiPathKey;
+  private String apiPathValue;
+
+  public String getApiPathKey() {
+    return apiPathKey;
+  }
+
+  public void setApiPathKey(String apiPathKey) {
+    this.apiPathKey = apiPathKey;
+  }
+
+  public String getApiPathValue() {
+    return apiPathValue;
+  }
+
+  public void setApiPathValue(String apiPathValue) {
+    this.apiPathValue = apiPathValue;
+  }
+
+}
