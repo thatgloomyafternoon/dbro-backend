0e9329a28a9da87204f7273cfe426f69c28e7f12
fw <thatgloomyafternoon@gmail.com>
2026-03-12 10:03:18 +0700

    progress

---
+++ src/main/java/com/fw/dbro/backend/dto/SysconfigDTO.java
@@ -0,0 +1,72 @@
+package com.fw.dbro.backend.dto;
+
+import java.time.ZonedDateTime;
+import java.util.UUID;
+
+public class SysconfigDTO {
+
+  private UUID id;
+  private ZonedDateTime createdDate;
+  private String createdBy;
+  private ZonedDateTime lastModifiedDate;
+  private String lastModifiedBy;
+  private String key;
+  private String value;
+
+  public UUID getId() {
+    return id;
+  }
+
+  public void setId(UUID id) {
+    this.id = id;
+  }
+
+  public ZonedDateTime getCreatedDate() {
+    return createdDate;
+  }
+
+  public void setCreatedDate(ZonedDateTime createdDate) {
+    this.createdDate = createdDate;
+  }
+
+  public String getCreatedBy() {
+    return createdBy;
+  }
+
+  public void setCreatedBy(String createdBy) {
+    this.createdBy = createdBy;
+  }
+
+  public ZonedDateTime getLastModifiedDate() {
+    return lastModifiedDate;
+  }
+
+  public void setLastModifiedDate(ZonedDateTime lastModifiedDate) {
+    this.lastModifiedDate = lastModifiedDate;
+  }
+
+  public String getLastModifiedBy() {
+    return lastModifiedBy;
+  }
+
+  public void setLastModifiedBy(String lastModifiedBy) {
+    this.lastModifiedBy = lastModifiedBy;
+  }
+
+  public String getKey() {
+    return key;
+  }
+
+  public void setKey(String key) {
+    this.key = key;
+  }
+
+  public String getValue() {
+    return value;
+  }
+
+  public void setValue(String value) {
+    this.value = value;
+  }
+
+}
